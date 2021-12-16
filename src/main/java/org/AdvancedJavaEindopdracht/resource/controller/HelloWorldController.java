463bc97345720f376e3e089577138e1e3dfb5d6a
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2021-12-16 14:25:20 +0100

    Update HelloWorldController.java

--- src/main/java/org/AdvancedJavaEindopdracht/resource/controller/HelloWorldController.java
+++ src/main/java/org/AdvancedJavaEindopdracht/resource/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package org.AdvancedJavaEindopdracht.resource;
+package org.AdvancedJavaEindopdracht.resource.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
