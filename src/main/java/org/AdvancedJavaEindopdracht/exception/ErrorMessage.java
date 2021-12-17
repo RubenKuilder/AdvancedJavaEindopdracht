5f80bdf90b49e9ba717062369322e4e6ce833cbd
thijsrijkers <thijsrijkers@live.nl>
2021-12-17 11:32:35 +0100

    +Pushing pom change for other branches

---
+++ src/main/java/org/AdvancedJavaEindopdracht/exception/ErrorMessage.java
@@ -0,0 +1,12 @@
+package org.AdvancedJavaEindopdracht.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private String error;
+
+}
