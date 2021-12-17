63f17bc2ada60490a04655f2dca8a426e3c9c798
Ruben <rubenkuilder@gmail.com>
2021-12-17 12:03:51 +0100

    Changed methor name

--- src/main/java/org/AdvancedJavaEindopdracht/exception/PutExceptionHandler.java
+++ src/main/java/org/AdvancedJavaEindopdracht/exception/PutExceptionHandler.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 public class PutExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoChangeException.class)
-    public ErrorMessage handleAccessDeniedException(NoChangeException exception, HttpServletRequest request) {
+    public ErrorMessage handleNoChangeException(NoChangeException exception, HttpServletRequest request) {
         String response = String.format(
                 "I have the message '%s' for %s",
                 exception.getMessage(),
