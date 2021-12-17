091e7e8fcbafeb96e6d316dad9005b4c71ea9960
Ruben <rubenkuilder@gmail.com>
2021-12-17 11:52:07 +0100

    Created GetExceptionHandler

---
+++ src/main/java/org/AdvancedJavaEindopdracht/exception/get/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package org.AdvancedJavaEindopdracht.exception.get;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
