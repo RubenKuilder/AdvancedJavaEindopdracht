f5d87da55f41ca22b8f6cc78881e9ddb6f5a6277
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2022-01-10 14:54:35 +0100

    Fixed Token Verification

---
+++ src/main/java/org/AdvancedJavaEindopdracht/security/UnauthenticatedHandler.java
@@ -0,0 +1,24 @@
+package org.AdvancedJavaEindopdracht.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * We use this class to send a response if
+ */
+public class UnauthenticatedHandler implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse,
+            AuthenticationException e) throws IOException {
+        httpServletResponse.setStatus(401);
+        httpServletResponse.getWriter().write("{\"message\":\"You're not authenticated.\"}");
+    }
+}
