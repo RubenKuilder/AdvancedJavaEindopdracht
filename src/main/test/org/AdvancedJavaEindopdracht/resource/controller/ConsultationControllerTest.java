934c2625bd72d8ab200fff94c0636c9cbc81baa2
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2022-01-12 13:06:33 +0100

    update

---
+++ src/main/test/org/AdvancedJavaEindopdracht/resource/controller/ConsultationControllerTest.java
@@ -0,0 +1,85 @@
+package org.AdvancedJavaEindopdracht.resource.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringJUnitWebConfig(classes = com.configuration.DatabaseConfigTest.class)
+@Transactional
+class ConsultationControllerTest
+{
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup()
+    {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webContext).build();
+    }
+
+
+    @Test
+    void getAllConsultation() throws Exception
+    {
+        this.mockMvc.perform(get("/consultation").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.[0].startDateTime").value("08-11-2021 00:00:00"))
+                .andExpect(jsonPath("$.[0].endDateTime").value("08-12-2022 00:00:00"))
+                .andExpect(jsonPath("$.[1].startDateTime").value("01-01-2021 00:00:00"))
+                .andExpect(jsonPath("$.[1].endDateTime").value("01-01-2022 00:00:00"));
+    }
+
+    @Test
+    void getById() throws Exception
+    {
+        this.mockMvc.perform(get("/consultation/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.startDateTime").value("08-11-2021 00:00:00"))
+                .andExpect(jsonPath("$.endDateTime").value("08-12-2022 00:00:00"));
+    }
+
+    @Test
+    void postConsultation() throws Exception
+    {
+        this.mockMvc.perform(post("/consultation")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"soundOn\":false,\"switchTime\":\"00:12:03\"}"))
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.soundOn").value(false))
+                .andExpect(jsonPath("$.switchTime").value("00:12:03"));
+    }
+
+    @Test
+    void putConsultation() throws Exception
+    {
+
+    }
+
+    @Test
+    void patchConsultation() throws Exception
+    {
+
+    }
+
+    @Test
+    void deleteConsultation() throws Exception
+    {
+
+    }
+}
