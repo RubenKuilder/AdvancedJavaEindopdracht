0a6b378807eaeffa28fdcfe739e32af2c09e0b4c
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2022-01-12 11:07:06 +0100

    test fixed

--- src/main/test/org/AdvancedJavaEindopdracht/resource/controller/LoginControllerTest.java
+++ src/main/test/org/AdvancedJavaEindopdracht/resource/controller/LoginControllerTest.java
@@ -1,5 +1,8 @@
 package org.AdvancedJavaEindopdracht.resource.controller;
 
+import org.AdvancedJavaEindopdracht.security.JWTProvider;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -20,6 +24,9 @@ class LoginControllerTest
     @Autowired
     private WebApplicationContext webContext;
 
+    @Autowired
+    private JWTProvider jwtProvider;
+
     private MockMvc mockMvc;
 
     @BeforeEach
@@ -31,10 +38,12 @@ class LoginControllerTest
     @Test
     void login() throws Exception
     {
-        this.mockMvc.perform(post("/authenticate")
+        MvcResult mvcResult = this.mockMvc.perform(post("/authenticate")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"Madlyaza\",\"password\":\"Password\"}"))
                 .andExpect(status().isOk())
                 .andReturn();
+
+        Assertions.assertTrue(jwtProvider.validateToken(mvcResult.getResponse().getContentAsString()));
     }
 }
