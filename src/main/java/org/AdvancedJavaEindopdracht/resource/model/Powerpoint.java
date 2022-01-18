6f193e35bdf05f7d719941817cc82959c2552431
thijsrijkers <thijsrijkers@live.nl>
2022-01-18 14:59:41 +0100

    +fixes for front end

--- src/main/java/org/AdvancedJavaEindopdracht/resource/model/Powerpoint.java
+++ src/main/java/org/AdvancedJavaEindopdracht/resource/model/Powerpoint.java
@@ -15,7 +15,7 @@ public class Powerpoint {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @ManyToOne(fetch = FetchType.EAGER,cascade=CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
