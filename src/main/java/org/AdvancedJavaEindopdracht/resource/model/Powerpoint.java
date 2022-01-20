9e949fc56201c03d9770ce743210c91752711abe
Ruben <rubenkuilder@gmail.com>
2022-01-20 14:57:16 +0100

    Fixed cascadeType errors

--- src/main/java/org/AdvancedJavaEindopdracht/resource/model/Powerpoint.java
+++ src/main/java/org/AdvancedJavaEindopdracht/resource/model/Powerpoint.java
@@ -15,7 +15,7 @@ public class Powerpoint {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
