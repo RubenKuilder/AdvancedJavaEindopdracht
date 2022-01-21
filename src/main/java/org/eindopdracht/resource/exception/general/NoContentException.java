eaf8f8671c38ddbadf173b579549c48e2ac88cb8
thijsrijkers <thijsrijkers@live.nl>
2022-01-21 17:16:14 +0100

    no message

--- src/main/java/org/eindopdracht/resource/exception/general/NoContentException.java
+++ src/main/java/org/eindopdracht/resource/exception/general/NoContentException.java
@@ -2,6 +2,6 @@ package org.eindopdracht.resource.exception.general;
 
 public class NoContentException extends RuntimeException {
     public NoContentException(String message) {
-        super("No Content - " + message);
+        super("No Content has been found on - " + message);
     }
 }
